fa00ee971d9c7add1554efae6dc8522436fa3a30
David Sansome <davidsansome@gmail.com>
2010-03-23 23:11:46 +0000

    Add license headers to all our source files

--- src/enginebase.cpp
+++ src/enginebase.cpp
@@ -1,3 +1,19 @@
+/* This file is part of Clementine.
+
+   Clementine is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Clementine is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Clementine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 //Copyright: (C) 2003 Mark Kretschmann
 //           (C) 2004,2005 Max Howell, <max.howell@methylblue.com>
 //License:   See COPYING
