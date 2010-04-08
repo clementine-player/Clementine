b6fd4dd3ac3ee614d10fb49a334941697832c1c2
David Sansome <davidsansome@gmail.com>
2010-04-08 21:32:43 +0000

    Fix style in gstequalizer, and fix unused warnings from the elusively named irr_cf.h

--- src/engines/gstequalizer.h
+++ src/engines/gstequalizer.h
@@ -16,66 +16,62 @@ G_BEGIN_DECLS
 #define EQ_CHANNELS 2
 
 #define GST_TYPE_EQUALIZER \
-  (gst_equalizer_get_type())
+    (gst_equalizer_get_type())
 #define GST_EQUALIZER(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_EQUALIZER,GstEqualizer))
+    (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_EQUALIZER,GstEqualizer))
 #define GST_EQUALIZER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_EQUALIZER,GstEqualizerClass))
+    (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_EQUALIZER,GstEqualizerClass))
 #define GST_IS_EQUALIZER(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_EQUALIZER))
+    (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_EQUALIZER))
 #define GST_IS_EQUALIZER_CLASS(obj) \
-  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_EQUALIZER))
+    (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_EQUALIZER))
 
-typedef struct _GstEqualizer GstEqualizer;
+    typedef struct _GstEqualizer GstEqualizer;
 typedef struct _GstEqualizerClass GstEqualizerClass;
 
 // Floating point
-typedef struct
-{
-    float beta;
-    float alpha;
-    float gamma;
+typedef struct {
+  float beta;
+  float alpha;
+  float gamma;
 } sIIRCoefficients;
 
 /* Coefficient history for the IIR filter */
-typedef struct
-{
-    float x[3]; /* x[n], x[n-1], x[n-2] */
-    float y[3]; /* y[n], y[n-1], y[n-2] */
+typedef struct {
+  float x[3]; /* x[n], x[n-1], x[n-2] */
+  float y[3]; /* y[n], y[n-1], y[n-2] */
 } sXYData;
 
 
-struct _GstEqualizer
-{
-    // Do not remove
-    GstBaseTransform element;
+struct _GstEqualizer {
+  // Do not remove
+  GstBaseTransform element;
 
-    GstPad *srcpad;
-    GstPad *sinkpad;
+  GstPad *srcpad;
+  GstPad *sinkpad;
 
-    int samplerate;
-    int channels;
+  int samplerate;
+  int channels;
 
-    // Properties
-    bool active;
-    // Gain for each band
-    // values should be between -0.2 and 1.0
-    float gain[EQ_MAX_BANDS][EQ_CHANNELS] __attribute__((aligned));
-    // Volume gain
-    // values should be between 0.0 and 1.0
-    float preamp[EQ_CHANNELS] __attribute__((aligned));
+  // Properties
+  bool active;
+  // Gain for each band
+  // values should be between -0.2 and 1.0
+  float gain[EQ_MAX_BANDS][EQ_CHANNELS] __attribute__((aligned));
+  // Volume gain
+  // values should be between 0.0 and 1.0
+  float preamp[EQ_CHANNELS] __attribute__((aligned));
 
-    // Coefficients
-    sIIRCoefficients* iir_cf;
+  // Coefficients
+  sIIRCoefficients* iir_cf;
 
-    sXYData data_history[EQ_MAX_BANDS][EQ_CHANNELS] __attribute__((aligned));
+  sXYData data_history[EQ_MAX_BANDS][EQ_CHANNELS] __attribute__((aligned));
 };
 
-struct _GstEqualizerClass
-{
-    GstBaseTransformClass parent_class;
+struct _GstEqualizerClass {
+  GstBaseTransformClass parent_class;
 
-    /* signals */
+  /* signals */
 };
 
 void gst_equalizer_set_property( GObject * object, guint prop_id, const GValue * value, GParamSpec * pspec );
