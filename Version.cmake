111f0d066b2c2e4aafbd7fb7fd99b2342efd1ba7
David Sansome <davidsansome@gmail.com>
2010-05-06 13:05:00 +0000

    Version 0.3 of ubuntu and fedora packages

--- Version.cmake
+++ Version.cmake
@@ -1,14 +1,14 @@
 # Change this file when releasing a new version.
 # You'll also need to add a new changelog entry in:
 #   debian/changelog
-#   dist/clementine.spec
+#   dist/clementine.spec.in
 
 # Version numbers.  For m.n rc, use m.(n-1).99
 set(CLEMENTINE_VERSION_MAJOR 0)
-set(CLEMENTINE_VERSION_MINOR 2)
-set(CLEMENTINE_VERSION_PATCH 99)
-set(CLEMENTINE_VERSION_STRING "0.3 rc3")       # Displayed in GUIs
-set(CLEMENTINE_VERSION_SHORT_STRING "0.3-rc3") # Used in filenames
+set(CLEMENTINE_VERSION_MINOR 3)
+set(CLEMENTINE_VERSION_PATCH 0)
+set(CLEMENTINE_VERSION_STRING "0.3")       # Displayed in GUIs
+set(CLEMENTINE_VERSION_SHORT_STRING "0.3") # Used in filenames
 
 
 if (CLEMENTINE_VERSION_PATCH)
