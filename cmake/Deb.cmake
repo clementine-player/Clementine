64ee9749af8e7bdb4101c79a83c49bfa1568993d
David Sansome <davidsansome@gmail.com>
2010-09-05 11:48:56 +0000

    Only use the ~rnnnn notation for debian packages, use .rnnnn for rpms

--- cmake/Deb.cmake
+++ cmake/Deb.cmake
@@ -2,7 +2,7 @@ set(DEB_ARCH amd64 CACHE STRING "Architecture of the deb file")
 
 add_custom_target(deb
   COMMAND dpkg-buildpackage -b -uc -us
-  COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/../clementine_${CLEMENTINE_VERSION}_${DEB_ARCH}.deb
+  COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/../clementine_${CLEMENTINE_DEB_VERSION}_${DEB_ARCH}.deb
                                    ${CMAKE_BINARY_DIR}/clementine.deb
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 )
