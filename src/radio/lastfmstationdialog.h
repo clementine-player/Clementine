dcd18aacdc4288b8e01f08ee4db808e58e0210ae
David Sansome <davidsansome@gmail.com>
2010-10-07 19:59:48 +0000

    Revert r2095 - it doesn't compile with boost 1.40

--- src/radio/lastfmstationdialog.h
+++ src/radio/lastfmstationdialog.h
@@ -37,9 +37,6 @@ class LastFMStationDialog : public QDialog {
   void SetType(Type type);
   QString content() const;
 
- public slots:
-  virtual void accept();
-
  private:
   Ui_LastFMStationDialog* ui_;
 };
