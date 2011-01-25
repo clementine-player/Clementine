4e9aef16cb999ad4a6d1e0d62c64e0b2cfd207ac
Andrea Decorte <adecorte@gmail.com>
2011-01-25 20:13:28 +0000

    forgot to add these files, sorry

---
+++ src/widgets/clickablelabel.h
@@ -0,0 +1,36 @@
+/* This file is part of Clementine.
+   Copyright 2010, Andrea Decorte <adecorte@gmail.com>
+
+   Clementine is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Clementine is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Clementine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CLICKABLELABEL_H
+#define CLICKABLELABEL_H
+
+#include <QLabel>
+
+class ClickableLabel : public QLabel {
+  Q_OBJECT
+
+public:
+  ClickableLabel(QWidget* parent = 0);
+
+signals:
+  void Clicked();
+
+protected:
+  void mousePressEvent(QMouseEvent* event);
+};
+
+#endif // CLICKABLELABEL_H
