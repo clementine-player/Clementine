4ecd63f1377ef6aa2634eaf29c2c0b8863dd0300
David Sansome <davidsansome@gmail.com>
2011-02-21 20:33:44 +0000

    Add some hacky shell scripts to svn that update and merge translations from Launchpad

---
+++ dist/mergetranslations/init-www.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+bzr branch lp:~me-davidsansome/clementine/exported-po www-bzr
+svn co https://clementine-player.googlecode.com/svn/www www-svn
---
+++ dist/mergetranslations/init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+bzr branch lp:~me-davidsansome/clementine/exported-po bzr
+svn co https://clementine-player.googlecode.com/svn/trunk svn
