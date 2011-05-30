143308d2d20af91e01af02bbb135c760f97dad2a
David Sansome <davidsansome@gmail.com>
2011-05-30 14:53:18 +0000

    Fix the shutdown script to work with PythonQt

--- scripts/shutdown/main.py
+++ scripts/shutdown/main.py
@@ -1,27 +1,30 @@
 import clementine
 
-from PyQt4.QtGui import QAction
+from PythonQt.QtGui import QAction
 
+import logging
 import sys
+
+LOGGER = logging.getLogger("system-shutdown")
+
 
 class Plugin:
   def __init__(self):
     self.enabled = False
-    clementine.player.PlaylistFinished.connect(self.PlaylistFinished)
+    clementine.player.connect("PlaylistFinished()", self.PlaylistFinished)
     self.action = QAction("Shutdown at end", None)
     self.action.setCheckable(True)
-    self.action.triggered.connect(self.Enabled)
+    self.action.connect("triggered(bool)", self.Enabled)
     clementine.ui.AddAction("playlist_menu", self.action)
 
-
-  # Slots
   def PlaylistFinished(self):
     if self.enabled:
-      print "Reached the end of the playlist - shutting down."
+      LOGGER.info("Reached the end of the playlist - shutting down")
       sys.exit(0)
 
   def Enabled(self, enabled):
-    print "Shutdown at end of playlist enabled: %s" % enabled
+    LOGGER.info("Shutdown at end of playlist enabled: %s" % str(enabled))
     self.enabled = enabled
+
 
 plugin = Plugin()
