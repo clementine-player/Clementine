606b88bb3c9204827f2037bad31d57acecadfded
David Sansome <me@davidsansome.com>
2011-10-30 19:12:57 +0000

    Remove the scripts to merge translations from launchpad

--- dist/mergetranslations/init-www.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-bzr branch lp:~me-davidsansome/clementine/exported-po www-bzr
-svn co https://clementine-player.googlecode.com/svn/www www-svn
--- dist/mergetranslations/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-bzr branch lp:~me-davidsansome/clementine/exported-po bzr
-svn co https://clementine-player.googlecode.com/svn/trunk svn
