c0ac798a7154685c9c83ce353a396f38c50f36bf
David Sansome <me@davidsansome.com>
2011-12-02 14:47:25 +0000

    Create a 1.0 release branch, and rework the way version numbers are done everywhere.

--- cmake/Rpm.cmake
+++ cmake/Rpm.cmake
@@ -6,14 +6,14 @@ set(RPM_ARCH x86_64 CACHE STRING "Architecture of the rpm file")
 
 add_custom_target(rpm
   COMMAND ${CMAKE_SOURCE_DIR}/dist/maketarball.sh
-  COMMAND ${CMAKE_COMMAND} -E copy clementine-${CLEMENTINE_VERSION_RPM}.tar.gz ${RPMBUILD_DIR}/SOURCES/
+  COMMAND ${CMAKE_COMMAND} -E copy clementine-${CLEMENTINE_VERSION_RPM_V}.tar.gz ${RPMBUILD_DIR}/SOURCES/
   COMMAND rpmbuild -bs ${CMAKE_SOURCE_DIR}/dist/clementine.spec
   COMMAND ${MOCK_COMMAND}
       --verbose
       --root=${MOCK_CHROOT}
       --resultdir=${CMAKE_BINARY_DIR}/mock_result/
-      ${RPMBUILD_DIR}/SRPMS/clementine-${CLEMENTINE_VERSION_RPM}-1.${RPM_DISTRO}.src.rpm
+      ${RPMBUILD_DIR}/SRPMS/clementine-${CLEMENTINE_VERSION_RPM_V}-${CLEMENTINE_VERSION_RPM_R}.${RPM_DISTRO}.src.rpm
   COMMAND ${CMAKE_COMMAND} -E copy
-      ${CMAKE_BINARY_DIR}/mock_result/clementine-${CLEMENTINE_VERSION_RPM}-1.${RPM_DISTRO}.${RPM_ARCH}.rpm
-      ${CMAKE_BINARY_DIR}/clementine-${CLEMENTINE_VERSION_RPM}-1.${RPM_DISTRO}.${RPM_ARCH}.rpm
+      ${CMAKE_BINARY_DIR}/mock_result/clementine-${CLEMENTINE_VERSION_RPM_V}-${CLEMENTINE_VERSION_RPM_R}.${RPM_DISTRO}.${RPM_ARCH}.rpm
+      ${CMAKE_BINARY_DIR}/clementine-${CLEMENTINE_VERSION_RPM_V}-${CLEMENTINE_VERSION_RPM_R}.${RPM_DISTRO}.${RPM_ARCH}.rpm
 )
