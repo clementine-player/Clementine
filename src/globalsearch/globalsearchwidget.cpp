68a1ac545f6b20f3d2646c45efbfebaf149ad39b
Arnaud Bienner <arnaud.bienner@gmail.com>
2011-12-06 23:25:40 +0100

    Display global search results when focusing on global search box only if reason is 'mouseFocus' (cherry picked from commit 0322c2bbf1c2c1029ca701381ec10ec3940e7641)

--- src/globalsearch/globalsearchwidget.cpp
+++ src/globalsearch/globalsearchwidget.cpp
@@ -367,9 +367,13 @@ bool GlobalSearchWidget::EventFilterSearchWidget(QObject* o, QEvent* e) {
       return true;
     break;
 
-  case QEvent::FocusIn:
-    RepositionPopup();
+  case QEvent::FocusIn: {
+    QFocusEvent* fe = static_cast<QFocusEvent*>(e);
+    if (fe->reason() == Qt::MouseFocusReason) {
+      RepositionPopup();
+    }
     break;
+  }
 
   case QEvent::KeyPress: {
     QKeyEvent* ke = static_cast<QKeyEvent*>(e);
