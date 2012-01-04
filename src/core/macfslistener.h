96aa95bebd30ba550b0cec7ce7e88afa8fc19ca3
John Maguire <john.maguire@gmail.com>
2012-01-04 15:24:53 +0000

    Add filesystem watcher using Mac FSEvents API.

---
+++ src/core/macfslistener.h
@@ -0,0 +1,38 @@
+#ifndef MACFSLISTENER_H
+#define MACFSLISTENER_H
+
+#include <CoreServices/CoreServices.h>
+
+#include <QObject>
+#include <QSet>
+
+class MacFSListener : public QObject {
+  Q_OBJECT
+
+ public:
+  MacFSListener();
+  void Init();
+
+  void AddPath(const QString& path);
+
+ signals:
+  void PathChanged(const QString& path);
+
+ private:
+  void UpdateStream();
+
+  static void EventStreamCallback(
+      ConstFSEventStreamRef stream,
+      void* user_data,
+      size_t num_events,
+      void* event_paths,
+      const FSEventStreamEventFlags event_flags[],
+      const FSEventStreamEventId event_ids[]);
+
+  CFRunLoopRef run_loop_;
+  FSEventStreamRef stream_;
+
+  QSet<QString> paths_;
+};
+
+#endif
