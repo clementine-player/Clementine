aa20c2b226d140b508e44d96c9264094fc73bb69
John Maguire <john.maguire@gmail.com>
2012-01-09 11:24:34 +0000

    Deploy clementine-tagreader on mac.

--- dist/macdeploy.py
+++ dist/macdeploy.py
@@ -369,8 +369,9 @@ FixPlugin(FindGstreamerPlugin('gst-plugin-scanner'), '.')
 
 try:
   FixPlugin('clementine-spotifyblob', '.')
-except:
-  print 'Failed to find spotify blob'
+  FixPlugin('clementine-tagreader', '.')
+except Exception, e:
+  print 'Failed to find spotify blob: %s' % e
 
 for plugin in QT_PLUGINS:
   FixPlugin(FindQtPlugin(plugin), os.path.dirname(plugin))
