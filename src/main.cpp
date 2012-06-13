4da488d6f0f896598294be3902cb1f9858ed835c
John Maguire <john.maguire@gmail.com>
2012-06-13 15:50:58 +0200

    Force native windows on OS X to mostly fix focus issue with NSSearchField. See QTBUG-11401.

--- src/main.cpp
+++ src/main.cpp
@@ -332,6 +332,8 @@ int main(int argc, char *argv[]) {
   QCoreApplication::setAttribute(Qt::AA_DontShowIconsInMenus, false);
 #else
   QCoreApplication::setAttribute(Qt::AA_DontShowIconsInMenus, true);
+  // Fixes focus issue with NSSearchField, see QTBUG-11401
+  QCoreApplication::setAttribute(Qt::AA_NativeWindows, true);
 #endif
 
   // Set the permissions on the config file on Unix - it can contain passwords
