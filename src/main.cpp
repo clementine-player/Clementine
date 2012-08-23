4719a347fa69fb2ab26e6bbccaa2032993405787
John Maguire <john.maguire@gmail.com>
2012-08-23 12:06:55 +0200

    Blacklist nvidia driver 304.37 Fixes issue #3115

--- src/main.cpp
+++ src/main.cpp
@@ -447,7 +447,8 @@ int main(int argc, char *argv[]) {
   QFile self_maps("/proc/self/maps");
   if (self_maps.open(QIODevice::ReadOnly)) {
     QByteArray data = self_maps.readAll();
-    if (data.contains("libnvidia-tls.so.285.03") ||
+    if (data.contains("libnvidia-tls.so.304.37") ||
+        data.contains("libnvidia-tls.so.285.03") ||
         data.contains("libnvidia-tls.so.280.13") ||
         data.contains("libnvidia-tls.so.275.28") ||
         data.contains("libnvidia-tls.so.275.19")) {
