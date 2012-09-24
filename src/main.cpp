c2723008a22c5a4c4e307ba9a278fd2162ef6bdb
Arnaud Bienner <arnaud.bienner@gmail.com>
2012-09-24 20:54:46 +0200

    Blacklist all NVidia drivers

--- src/main.cpp
+++ src/main.cpp
@@ -447,11 +447,7 @@ int main(int argc, char *argv[]) {
   QFile self_maps("/proc/self/maps");
   if (self_maps.open(QIODevice::ReadOnly)) {
     QByteArray data = self_maps.readAll();
-    if (data.contains("libnvidia-tls.so.304.37") ||
-        data.contains("libnvidia-tls.so.285.03") ||
-        data.contains("libnvidia-tls.so.280.13") ||
-        data.contains("libnvidia-tls.so.275.28") ||
-        data.contains("libnvidia-tls.so.275.19")) {
+    if (data.contains("libnvidia-tls.so.")) {
       qLog(Warning) << "Exiting immediately to work around NVIDIA driver bug";
       _exit(ret);
     }
