a80539d05b2286aa174d48ccd819455f21876405
David Sansome <me@davidsansome.com>
2012-10-27 17:12:18 -0700

    Add taglib-1.8 to 3rdparty (cherry picked from commit 66ce3b440843393f89c46e93a623b19701ab63af)

---
+++ 3rdparty/taglib/toolkit/tiostream.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+    copyright            : (C) 2011 by Lukas Lalinsky
+    email                : lalinsky@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Lesser General Public License version   *
+ *   2.1 as published by the Free Software Foundation.                     *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful, but   *
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA         *
+ *   02110-1301  USA                                                       *
+ *                                                                         *
+ *   Alternatively, this file is available under the Mozilla Public        *
+ *   License Version 1.1.  You may obtain a copy of the License at         *
+ *   http://www.mozilla.org/MPL/                                           *
+ ***************************************************************************/
+
+#include "tiostream.h"
+
+using namespace TagLib;
+
+////////////////////////////////////////////////////////////////////////////////
+// public members
+////////////////////////////////////////////////////////////////////////////////
+
+IOStream::IOStream()
+{
+}
+
+IOStream::~IOStream()
+{
+}
+
+void IOStream::clear()
+{
+}
+
