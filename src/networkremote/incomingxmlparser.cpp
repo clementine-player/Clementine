6b8d9be8c4d8247a977c961962c7b0bdbd258443
asfa194 <asfa194@gmail.com>
2013-01-02 15:03:11 +0000

    remove header comment in wrong place

--- src/networkremote/incomingxmlparser.cpp
+++ src/networkremote/incomingxmlparser.cpp
@@ -80,22 +80,7 @@ void IncomingXmlParser::Parse(QString *xml_data) {
   qLog(Debug) << "Action = " << action;
   if (action == RemoteXmlTags::CLIENT_CONNTECTED) {
     emit SendClementineInfos();
-    emit SendFirstData()/* This file is part of Clementine.
-       Copyright 2012, David Sansome <me@davidsansome.com>
-
-       Clementine is free software: you can redistribute it and/or modify
-       it under the terms of the GNU General Public License as published by
-       the Free Software Foundation, either version 3 of the License, or
-       (at your option) any later version.
-
-       Clementine is distributed in the hope that it will be useful,
-       but WITHOUT ANY WARRANTY; without even the implied warranty of
-       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-       GNU General Public License for more details.
-
-       You should have received a copy of the GNU General Public License
-       along with Clementine.  If not, see <http://www.gnu.org/licenses/>.
-    */;
+    emit SendFirstData();
   } else if (action == RemoteXmlTags::CLIENT_DISCONNECT) {
     close_connection_ = true;
   } else if (action == RemoteXmlTags::REQUEST_PLAYLISTS) {
