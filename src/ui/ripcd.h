74256ee5ff3760837c2fbcfbbb2747710b0e254c
asiviero <altsiviero@gmail.com>
2014-01-11 01:32:08 -0200

    Updating file headers

--- src/ui/ripcd.h
+++ src/ui/ripcd.h
@@ -1,11 +1,19 @@
-/********************************************************************************
- ** Form generated from reading UI file 'ripcd.ui'
- **
- ** Created: Fri Dec 27 02:11:12 2013
- **      by: Qt User Interface Compiler version 4.8.1
- **
- ** WARNING! All changes made in this file will be lost when recompiling UI file!
- ********************************************************************************/
+/* This file is part of Clementine.
+   Copyright 2014, Andre Siviero <altsiviero@gmail.com>
+
+   Clementine is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Clementine is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Clementine.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef RIPCD_H
 #define RIPCD_H
