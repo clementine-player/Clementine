20befa439c762a57bc43f62d3b19e31696b550fb
Krzysztof A. Sobiecki <sobkas@gmail.com>
2014-01-28 04:31:06 +0100

    Add a space

--- src/podcasts/podcastdownloader.cpp
+++ src/podcasts/podcastdownloader.cpp
@@ -131,7 +131,7 @@ void PodcastDownloader::FinishAndDelete(Task* task) {
   Song song = task->episode.ToSong(podcast);
   downloading_episode_ids_.remove(task->episode.database_id());
   emit ProgressChanged(task->episode, Finished, 0);
-  //I didn't ecountered even a single podcast with corect metadata
+  // I didn't ecountered even a single podcast with a corect metadata
   TagReaderClient::Instance()->SaveFileBlocking(task->file->fileName(), song);
   delete task;
 
