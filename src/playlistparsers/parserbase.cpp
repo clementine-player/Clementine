7a46629d973af57e8a7cd791a750a3b43d9a0a16
Alan Briolat <alan.briolat@gmail.com>
2014-08-12 21:33:18 +0100

    Fix a stupid missed semicolon
    I appear to be suffering from too much Python...

--- src/playlistparsers/parserbase.cpp
+++ src/playlistparsers/parserbase.cpp
@@ -49,7 +49,7 @@ void ParserBase::LoadSong(const QString& filename_or_url, qint64 beginning,
   // Clementine always wants / separators internally.  Using
   // QDir::fromNativeSeparators() only works on the same platform the playlist
   // was created on/for, using replace() lets playlists work on any platform.
-  filename = filename.replace('\\', '/')
+  filename = filename.replace('\\', '/');
 
   // Make the path absolute
   if (!QDir::isAbsolutePath(filename)) {
