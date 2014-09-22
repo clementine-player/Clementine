1c1ea31f494f6d8516c69432a276dfa8001dbd53
David Sansome <me@davidsansome.com>
2014-09-23 00:02:33 +1000

    Remove bits of sha2 not used for sha256

--- 3rdparty/sha2/sha2.h
+++ 3rdparty/sha2/sha2.h
@@ -48,12 +48,6 @@ namespace clementine_sha2 {
 static const int SHA256_BLOCK_LENGTH = 64;
 static const int SHA256_DIGEST_LENGTH = 32;
 static const int SHA256_DIGEST_STRING_LENGTH = (SHA256_DIGEST_LENGTH * 2 + 1);
-static const int SHA384_BLOCK_LENGTH = 128;
-static const int SHA384_DIGEST_LENGTH = 48;
-static const int SHA384_DIGEST_STRING_LENGTH = (SHA384_DIGEST_LENGTH * 2 + 1);
-static const int SHA512_BLOCK_LENGTH = 128;
-static const int SHA512_DIGEST_LENGTH = 64;
-static const int SHA512_DIGEST_STRING_LENGTH = (SHA512_DIGEST_LENGTH * 2 + 1);
 
 
 /*** SHA-256/384/512 Context Structures *******************************/
@@ -72,13 +66,6 @@ typedef struct _SHA256_CTX {
 	u_int64_t	bitcount;
 	u_int8_t	buffer[SHA256_BLOCK_LENGTH];
 } SHA256_CTX;
-typedef struct _SHA512_CTX {
-	u_int64_t	state[8];
-	u_int64_t	bitcount[2];
-	u_int8_t	buffer[SHA512_BLOCK_LENGTH];
-} SHA512_CTX;
-
-typedef SHA512_CTX SHA384_CTX;
 
 
 void SHA256_Init(SHA256_CTX *);
@@ -86,18 +73,6 @@ void SHA256_Update(SHA256_CTX*, const u_int8_t*, size_t);
 void SHA256_Final(u_int8_t[SHA256_DIGEST_LENGTH], SHA256_CTX*);
 char* SHA256_End(SHA256_CTX*, char[SHA256_DIGEST_STRING_LENGTH]);
 char* SHA256_Data(const u_int8_t*, size_t, char[SHA256_DIGEST_STRING_LENGTH]);
-
-void SHA384_Init(SHA384_CTX*);
-void SHA384_Update(SHA384_CTX*, const u_int8_t*, size_t);
-void SHA384_Final(u_int8_t[SHA384_DIGEST_LENGTH], SHA384_CTX*);
-char* SHA384_End(SHA384_CTX*, char[SHA384_DIGEST_STRING_LENGTH]);
-char* SHA384_Data(const u_int8_t*, size_t, char[SHA384_DIGEST_STRING_LENGTH]);
-
-void SHA512_Init(SHA512_CTX*);
-void SHA512_Update(SHA512_CTX*, const u_int8_t*, size_t);
-void SHA512_Final(u_int8_t[SHA512_DIGEST_LENGTH], SHA512_CTX*);
-char* SHA512_End(SHA512_CTX*, char[SHA512_DIGEST_STRING_LENGTH]);
-char* SHA512_Data(const u_int8_t*, size_t, char[SHA512_DIGEST_STRING_LENGTH]);
 
 }  // namespace clementine_sha2
 
