51fdb27771dc9aca14b7494ed0e95b3223215077
Ville Ranki <ville.ranki@nomovok.com>
2014-09-30 16:40:18 +0300

    Added packaging script

---
+++ dist/create_debian_package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#
+# Builds the debian package of clementine.
+# Modify arch and dist variables if needed.
+#
+
+
+cd ..
+mkdir -p bin
+cmake ../ -DWITH_DEBIAN=ON -DDEB_ARCH=amd64 -DDEB_DIST=ubuntu -DENABLE_SPOTIFY_BLOB=OFF
+make deb
+
