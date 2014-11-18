c3c730272140f621f91eaa769154a5434aed6781
Ivan Leontiev <leont.ivan@gmail.com>
2014-11-19 02:57:55 +0300

    Fix vkservice with visualisations crash

--- src/internet/vkservice.cpp
+++ src/internet/vkservice.cpp
@@ -1145,6 +1145,8 @@ Vreen::AudioItem VkService::GetAudioItemFromUrl(const QUrl& url) {
     Vreen::AudioItemListReply* song_request =
         audio_provider_->getAudiosByIds(song_id);
     emit StopWaiting();  // Stop all previous requests.
+    // To prevent object destruction in nested event loop
+    song_request->deleteLater();
     bool success = WaitForReply(song_request);
 
     if (success && !song_request->result().isEmpty()) {
@@ -1465,7 +1467,9 @@ void VkService::ClearStandardItem(QStandardItem* item) {
 bool VkService::WaitForReply(Vreen::Reply* reply) {
   QEventLoop event_loop;
   QTimer timeout_timer;
+  timeout_timer.setSingleShot(true);
   connect(this, SIGNAL(StopWaiting()), &timeout_timer, SLOT(stop()));
+  connect(this, SIGNAL(StopWaiting()), &event_loop, SLOT(quit()));
   connect(&timeout_timer, SIGNAL(timeout()), &event_loop, SLOT(quit()));
   connect(reply, SIGNAL(resultReady(QVariant)), &event_loop, SLOT(quit()));
   timeout_timer.start(10000);
