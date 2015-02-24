30f4e871cc12eee6bb67720a186a1111a2e7b709
vranki <ville.ranki@iki.fi>
2015-02-24 17:33:44 +0200

    Better debian packaging scrpt.

--- dist/create_debian_package.sh
+++ dist/create_debian_package.sh
@@ -6,7 +6,8 @@
 # Modify arch and dist variables if needed.
 #
 
-
+sudo apt-get install devscripts equivs
+sudo mk-build-deps --install ../debian/control
 mkdir -p ../bin
 cmake ../ -DWITH_DEBIAN=ON -DDEB_ARCH=amd64 -DDEB_DIST=ubuntu -DENABLE_SPOTIFY_BLOB=OFF
 make deb
