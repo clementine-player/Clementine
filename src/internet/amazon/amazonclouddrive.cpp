4cbe098b8325b6275f42774d62d34898e4a7a2b3
John Maguire <john.maguire@gmail.com>
2015-04-27 14:02:07 +0100

    Fix memory leak in Amazon Cloud Drive.

--- src/internet/amazon/amazonclouddrive.cpp
+++ src/internet/amazon/amazonclouddrive.cpp
@@ -62,7 +62,7 @@ AmazonCloudDrive::AmazonCloudDrive(Application* app, InternetModel* parent)
     : CloudFileService(app, parent, kServiceName, kServiceId,
                        QIcon(":/providers/amazonclouddrive.png"),
                        SettingsDialog::Page_AmazonCloudDrive),
-      network_(new NetworkAccessManager) {
+      network_(new NetworkAccessManager(this)) {
   app->player()->RegisterUrlHandler(new AmazonUrlHandler(this, this));
 }
 
