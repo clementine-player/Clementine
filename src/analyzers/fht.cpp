c102bf7fe6fb4fae7167b8c27c948cb4c8db6f8b
Mark Furneaux <mark@furneaux.ca>
2015-07-01 11:48:03 -0400

    Add "Psychedelic Colour" mode to all analyzers (Well, except Nyanalyzer and Rainbow dash because they are already colourful enough.)
    I have added functionality for any 2D analyzer to change any part of its colour palatte with the frequency content of the music, in the same way that Moodbars do.
    I find this gives the analyzer a sort of "third dimention".
    This is built into Analyzer::Base, so all analyzers can use it and override it as they please. I have thus added support for Block, Boom, Turbine, Sonogram, and Bar, however Boom and Block seem to look the best in my opinion.
    This is of course all optional and is toggled by a checkbox in the context menu for the analyzer, disabled by default.
    I have not been able to measure any increase in CPU activity with this enabled, even at 60fps.

--- src/analyzers/fht.cpp
+++ src/analyzers/fht.cpp
@@ -24,70 +24,69 @@
 #include <string.h>
 #include "fht.h"
 
-FHT::FHT(int n) : m_buf(0), m_tab(0), m_log(0) {
+FHT::FHT(int n) : buf_(0), tab_(0), log_(0) {
   if (n < 3) {
-    m_num = 0;
-    m_exp2 = -1;
+    num_ = 0;
+    exp2_ = -1;
     return;
   }
-  m_exp2 = n;
-  m_num = 1 << n;
+  exp2_ = n;
+  num_ = 1 << n;
   if (n > 3) {
-    m_buf = new float[m_num];
-    m_tab = new float[m_num * 2];
+    buf_ = new float[num_];
+    tab_ = new float[num_ * 2];
     makeCasTable();
   }
 }
 
 FHT::~FHT() {
-  delete[] m_buf;
-  delete[] m_tab;
-  delete[] m_log;
+  delete[] buf_;
+  delete[] tab_;
+  delete[] log_;
 }
 
 void FHT::makeCasTable(void) {
   float d, *costab, *sintab;
-  int ul, ndiv2 = m_num / 2;
+  int ul, ndiv2 = num_ / 2;
 
-  for (costab = m_tab, sintab = m_tab + m_num / 2 + 1, ul = 0; ul < m_num;
-       ul++) {
+  for (costab = tab_, sintab = tab_ + num_ / 2 + 1, ul = 0; ul < num_; ul++) {
     d = M_PI * ul / ndiv2;
     *costab = *sintab = cos(d);
 
     costab += 2, sintab += 2;
-    if (sintab > m_tab + m_num * 2) sintab = m_tab + 1;
+    if (sintab > tab_ + num_ * 2) sintab = tab_ + 1;
   }
 }
 
 float* FHT::copy(float* d, float* s) {
-  return static_cast<float*>(memcpy(d, s, m_num * sizeof(float)));
+  return static_cast<float*>(memcpy(d, s, num_ * sizeof(float)));
 }
 
 float* FHT::clear(float* d) {
-  return static_cast<float*>(memset(d, 0, m_num * sizeof(float)));
+  return static_cast<float*>(memset(d, 0, num_ * sizeof(float)));
 }
 
 void FHT::scale(float* p, float d) {
-  for (int i = 0; i < (m_num / 2); i++) *p++ *= d;
+  for (int i = 0; i < (num_ / 2); i++) *p++ *= d;
 }
 
 void FHT::ewma(float* d, float* s, float w) {
-  for (int i = 0; i < (m_num / 2); i++, d++, s++) *d = *d * w + *s * (1 - w);
+  for (int i = 0; i < (num_ / 2); i++, d++, s++) *d = *d * w + *s * (1 - w);
 }
 
 void FHT::logSpectrum(float* out, float* p) {
-  int n = m_num / 2, i, j, k, *r;
-  if (!m_log) {
-    m_log = new int[n];
+  int n = num_ / 2, i, j, k, *r;
+  if (!log_) {
+    log_ = new int[n];
     float f = n / log10(static_cast<double>(n));
-    for (i = 0, r = m_log; i < n; i++, r++) {
+    for (i = 0, r = log_; i < n; i++, r++) {
       j = static_cast<int>(rint(log10(i + 1.0) * f));
       *r = j >= n ? n - 1 : j;
     }
   }
   semiLogSpectrum(p);
   *out++ = *p = *p / 100;
-  for (k = i = 1, r = m_log; i < n; i++) {
+  for (k = i = 1, r = log_; i < n; i++) {
     j = *r++;
     if (i == j) {
       *out++ = p[i];
@@ -102,7 +101,7 @@ void FHT::logSpectrum(float* out, float* p) {
 void FHT::semiLogSpectrum(float* p) {
   float e;
   power2(p);
-  for (int i = 0; i < (m_num / 2); i++, p++) {
+  for (int i = 0; i < (num_ / 2); i++, p++) {
     e = 10.0 * log10(sqrt(*p * .5));
     *p = e < 0 ? 0 : e;
   }
@@ -110,31 +109,31 @@ void FHT::semiLogSpectrum(float* p) {
 
 void FHT::spectrum(float* p) {
   power2(p);
-  for (int i = 0; i < (m_num / 2); i++, p++)
+  for (int i = 0; i < (num_ / 2); i++, p++)
     *p = static_cast<float>(sqrt(*p * .5));
 }
 
 void FHT::power(float* p) {
   power2(p);
-  for (int i = 0; i < (m_num / 2); i++) *p++ *= .5;
+  for (int i = 0; i < (num_ / 2); i++) *p++ *= .5;
 }
 
 void FHT::power2(float* p) {
   int i;
   float* q;
-  _transform(p, m_num, 0);
+  _transform(p, num_, 0);
 
   *p = (*p * *p), *p += *p, p++;
 
-  for (i = 1, q = p + m_num - 2; i < (m_num / 2); i++, --q)
+  for (i = 1, q = p + num_ - 2; i < (num_ / 2); i++, --q)
     *p = (*p * *p) + (*q * *q), p++;
 }
 
 void FHT::transform(float* p) {
-  if (m_num == 8)
+  if (num_ == 8)
     transform8(p);
   else
-    _transform(p, m_num, 0);
+    _transform(p, num_, 0);
 }
 
 void FHT::transform8(float* p) {
@@ -173,19 +172,19 @@ void FHT::_transform(float* p, int n, int k) {
   int i, j, ndiv2 = n / 2;
   float a, *t1, *t2, *t3, *t4, *ptab, *pp;
 
-  for (i = 0, t1 = m_buf, t2 = m_buf + ndiv2, pp = &p[k]; i < ndiv2; i++)
+  for (i = 0, t1 = buf_, t2 = buf_ + ndiv2, pp = &p[k]; i < ndiv2; i++)
     *t1++ = *pp++, *t2++ = *pp++;
 
-  memcpy(p + k, m_buf, sizeof(float) * n);
+  memcpy(p + k, buf_, sizeof(float) * n);
 
   _transform(p, ndiv2, k);
   _transform(p, ndiv2, k + ndiv2);
 
-  j = m_num / ndiv2 - 1;
-  t1 = m_buf;
+  j = num_ / ndiv2 - 1;
+  t1 = buf_;
   t2 = t1 + ndiv2;
   t3 = p + k + ndiv2;
-  ptab = m_tab;
+  ptab = tab_;
   pp = p + k;
 
   a = *ptab++ * *t3++;
@@ -202,5 +201,5 @@ void FHT::_transform(float* p, int n, int k) {
     *t1++ = *pp + a;
     *t2++ = *pp++ - a;
   }
-  memcpy(p + k, m_buf, sizeof(float) * n);
+  memcpy(p + k, buf_, sizeof(float) * n);
 }
