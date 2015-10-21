dca37d83b0b18b93ed4100f789db4ed9cf7c92eb
vranki <ville.ranki@iki.fi>
2015-10-21 16:01:51 +0300

    Updated comments on debian package script.

--- dist/create_debian_package.sh
+++ dist/create_debian_package.sh
@@ -5,6 +5,11 @@
 # Builds the debian package of clementine.
 # Modify arch and dist variables if needed.
 #
+# Note: this is just a helper script to help
+# install all dependencies and build clementine
+# with one command. Do proper debian packaging
+# elsewhere.
+#
 
 sudo apt-get install devscripts equivs
 sudo mk-build-deps --install ../debian/control
