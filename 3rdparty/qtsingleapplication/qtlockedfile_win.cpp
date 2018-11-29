b60d390388b42ab41b61ebcf920dc6529f5d42f2
Jonas Kvinge <jonas@jkvinge.net>
2018-11-29 20:09:29 +0100

    Convert NULL to nullptr

--- 3rdparty/qtsingleapplication/qtlockedfile_win.cpp
+++ 3rdparty/qtsingleapplication/qtlockedfile_win.cpp
@@ -65,8 +65,8 @@ Qt::HANDLE QtLockedFile::getMutexHandle(int idx, bool doCreate)
 
     Qt::HANDLE mutex;
     if (doCreate) {
-        QT_WA( { mutex = CreateMutexW(NULL, FALSE, (WCHAR*)mname.utf16()); },
-               { mutex = CreateMutexA(NULL, FALSE, mname.toLocal8Bit().constData()); } );
+        QT_WA( { mutex = CreateMutexW(nullptr, FALSE, (WCHAR*)mname.utf16()); },
+               { mutex = CreateMutexA(nullptr, FALSE, mname.toLocal8Bit().constData()); } );
         if (!mutex) {
             qErrnoWarning("QtLockedFile::lock(): CreateMutex failed");
             return 0;
