c4031491f77703d6d64bf18eb4fa61fe4ba93ecd
Jonas Kvinge <jonas@jkvinge.net>
2019-01-27 12:22:53 +0100

    Fix this properly

--- src/devices/deviceview.cpp
+++ src/devices/deviceview.cpp
@@ -121,7 +121,7 @@ void DeviceItemDelegate::paint(QPainter* p, const QStyleOptionViewItem& opt,
         QVariant song_count = index.data(DeviceManager::Role_SongCount);
         if (song_count.isValid()) {
           int count = song_count.toInt();
-          status_text = tr("%1 song%2").arg(count).arg(count == 1 ? "" : "s");
+          status_text = (count == 1 ? tr("%1 song").arg(count) : tr("%1 songs").arg(count));
         } else {
           status_text = index.data(DeviceManager::Role_MountPath).toString();
         }
