2d80c46f52894071fab65a5e02d80f3cdb779a49
Jim Broadus <jbroadus@gmail.com>
2020-05-25 22:02:19 -0700

    console: Add a Qt debug tab
    Add a tab for Qt debug features. Initially provides access to dumpObjectTree() on
    Application and MainWindow objects. This dumps the object's child objects to the
    log.

--- src/ui/console.cpp
+++ src/ui/console.cpp
@@ -8,17 +8,24 @@
 
 #include "core/application.h"
 #include "core/database.h"
+#include "core/logging.h"
 
 Console::Console(Application* app, QWidget* parent)
     : QDialog(parent), app_(app) {
   ui_.setupUi(this);
   connect(ui_.database_run, SIGNAL(clicked()), SLOT(RunQuery()));
+  connect(ui_.qt_dump_button, SIGNAL(clicked()), SLOT(Dump()));
 
   QFont font("Monospace");
   font.setStyleHint(QFont::TypeWriter);
 
   ui_.database_output->setFont(font);
   ui_.database_query->setFont(font);
+
+  QList<QObject*> objs = GetTopLevelObjects();
+  for (QObject* obj : objs)
+    ui_.qt_dump_box->addItem(obj->objectName() + " object tree",
+                             obj->objectName());
 }
 
 void Console::RunQuery() {
@@ -44,4 +51,29 @@ void Console::RunQuery() {
 
   ui_.database_output->verticalScrollBar()->setValue(
       ui_.database_output->verticalScrollBar()->maximum());
+}
+
+void Console::Dump() {
+  QString item = ui_.qt_dump_box->currentData().toString();
+  QObject* obj = FindTopLevelObject(item);
+  if (obj == nullptr) {
+    qLog(Error) << "Object not found" << item;
+    return;
+  }
+  obj->dumpObjectTree();
+}
+
+QList<QObject*> Console::GetTopLevelObjects() {
+  QList<QObject*> objs;
+  objs << app_;
+  // The parent should be the main window.
+  if (parent() != nullptr) objs << parent();
+
+  return objs;
+}
+
+QObject* Console::FindTopLevelObject(QString& name) {
+  for (QObject* obj : GetTopLevelObjects())
+    if (obj->objectName() == name) return obj;
+  return nullptr;
 }
