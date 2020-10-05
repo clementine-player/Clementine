6343052a5ec71b06e0cf2fff09ea08a54c369790
Matthieu Bruel <Matthieu.Bruel@gmail.com>
2020-10-05 14:28:18 +0200

    Fix compilation issue

--- src/ui/mainwindow.h
+++ src/ui/mainwindow.h
@@ -225,8 +225,8 @@ class MainWindow : public QMainWindow, public PlatformInterface {
 #ifdef HAVE_LIBLASTFM
   void ScrobblingEnabledChanged(bool value);
   void ScrobbledRadioStream();
-#endif
   void Love();
+#endif
 
   void TaskCountChanged(int count);
 
@@ -254,8 +254,8 @@ class MainWindow : public QMainWindow, public PlatformInterface {
                         const QPersistentModelIndex& index);
 
   void ShowCoverManager();
-  bool IsLastFmEnabled();
 #ifdef HAVE_LIBLASTFM
+  bool IsLastFmEnabled();
   void CachedToScrobble();
   void ScrobbleError(int value);
 #endif
