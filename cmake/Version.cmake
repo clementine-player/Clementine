6f9b3ee6b66bb7dc6f62f547bf300d9f6fc86d2a
Matthieu Bruel <Matthieu.Bruel@gmail.com>
2020-11-23 15:04:01 +0100

    Revert version for pull request on Clementine

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -4,10 +4,10 @@
 set(CLEMENTINE_VERSION_MAJOR 1)
 set(CLEMENTINE_VERSION_MINOR 4)
 set(CLEMENTINE_VERSION_PATCH 0)
-set(CLEMENTINE_VERSION_PRERELEASE rc1ClemRemote)
+set(CLEMENTINE_VERSION_PRERELEASE rc2)
 
 # This should be set to OFF in a release branch
-set(INCLUDE_GIT_REVISION OFF)
+set(INCLUDE_GIT_REVISION ON)
 
 # Rules about version number comparison on different platforms:
 #   Debian:
