clementine-player/Clementine

commit 8b226c21710864c6f92d124647c65ba8dc4873eb

Update non optionnals sources to qt5

src/internet/core/oauthenticator.cpp
@@ -23,8 +23,10 @@
 #include <QDesktopServices>
 #include <QStringList>
 #include <QUrl>
-
-#include <qjson/parser.h>
+#include <QUrlQuery>
+#include <QJsonParseError>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 #include "core/closure.h"
 #include "core/logging.h"
@@ -49,24 +51,29 @@ void OAuthenticator::StartAuthorisation(const QString& oauth_endpoint,
   server->Listen();
 
   QUrl url = QUrl(oauth_endpoint);
-  url.addQueryItem("response_type", "code");
-  url.addQueryItem("client_id", client_id_);
+  QUrlQuery url_query;
+  url_query.addQueryItem("response_type", "code");
+  url_query.addQueryItem("client_id", client_id_);
   QUrl redirect_url;
+  QUrlQuery redirect_url_query;
 
   const QString port = QString::number(server->url().port());
 
   if (redirect_style_ == RedirectStyle::REMOTE) {
     redirect_url = QUrl(kRemoteURL);
-    redirect_url.addQueryItem("port", port);
+    redirect_url_query.addQueryItem("port", port);
   } else if (redirect_style_ == RedirectStyle::REMOTE_WITH_STATE) {
     redirect_url = QUrl(kRemoteURL);
-    url.addQueryItem("state", port);
+    url_query.addQueryItem("state", port);
   } else {
     redirect_url = server->url();
   }
 
-  url.addQueryItem("redirect_uri", redirect_url.toString());
-  url.addQueryItem("scope", scope);
+  url_query.addQueryItem("redirect_uri", redirect_url.toString());
+  url_query.addQueryItem("scope", scope);
+
+  url.setQuery(url_query);
+  redirect_url.setQuery(redirect_url_query);
 
   NewClosure(server, SIGNAL(Finished()), this, &OAuthenticator::RedirectArrived,
              server, redirect_url);
@@ -78,7 +85,7 @@ void OAuthenticator::RedirectArrived(LocalRedirectServer* server, QUrl url) {
   server->deleteLater();
   QUrl request_url = server->request_url();
   qLog(Debug) << Q_FUNC_INFO << request_url;
-  RequestAccessToken(request_url.queryItemValue("code").toUtf8(), url);
+  RequestAccessToken(QUrlQuery(request_url).queryItemValue("code").toUtf8(), url);
 }
 
 QByteArray OAuthenticator::ParseHttpRequest(const QByteArray& request) const {
@@ -126,19 +133,20 @@ void OAuthenticator::FetchAccessTokenFinished(QNetworkReply* reply) {
     return;
   }
 
-  QJson::Parser parser;
-  bool ok = false;
-  QVariantMap result = parser.parse(reply, &ok).toMap();
-  if (!ok) {
+  QJsonParseError error;
+  QJsonDocument json_document = QJsonDocument::fromJson(reply->readAll(), &error);
+
+  if (error.error != QJsonParseError::NoError) {
     qLog(Error) << "Failed to parse oauth reply";
     return;
   }
 
-  qLog(Debug) << result;
+  QJsonObject json_result = json_document.object();
+  qLog(Debug) << json_result;
 
-  access_token_ = result["access_token"].toString();
-  refresh_token_ = result["refresh_token"].toString();
-  SetExpiryTime(result["expires_in"].toInt());
+  access_token_ = json_result["access_token"].toString();
+  refresh_token_ = json_result["refresh_token"].toString();
+  SetExpiryTime(json_result["expires_in"].toInt());
 
   emit Finished();
 }
@@ -178,14 +186,13 @@ void OAuthenticator::SetExpiryTime(int expires_in_seconds) {
 
 void OAuthenticator::RefreshAccessTokenFinished(QNetworkReply* reply) {
   reply->deleteLater();
-  QJson::Parser parser;
-  bool ok = false;
 
-  QVariantMap result = parser.parse(reply, &ok).toMap();
-  access_token_ = result["access_token"].toString();
-  if (result.contains("refresh_token")) {
-    refresh_token_ = result["refresh_token"].toString();
+  QJsonObject json_result = QJsonDocument::fromJson(reply->readAll()).object();
+
+  access_token_ = json_result["access_token"].toString();
+  if (json_result.contains("refresh_token")) {
+    refresh_token_ = json_result["refresh_token"].toString();
   }
-  SetExpiryTime(result["expires_in"].toInt());
+  SetExpiryTime(json_result["expires_in"].toInt());
   emit Finished();
 }

commit 06243148798c34746a2e8615cca61bd3bac0866c

oauth: Fix redirect
Query params were not added to redirect url prior to inclusion in oauth
request. This change fixes OneDrive authentication.

Regression introduced in 8b226c2171

src/internet/core/oauthenticator.cpp
@@ -70,13 +70,14 @@ void OAuthenticator::StartAuthorisation(const QString& oauth_endpoint,
     redirect_url = server->url();
   }
 
+  redirect_url.setQuery(redirect_url_query);
+
   url_query.addQueryItem("redirect_uri", redirect_url.toString());
   if (!scope.isEmpty()) {  // Empty scope is valid for Dropbox.
     url_query.addQueryItem("scope", scope);
   }
 
   url.setQuery(url_query);
-  redirect_url.setQuery(redirect_url_query);
 
   NewClosure(server, SIGNAL(Finished()), this, &OAuthenticator::RedirectArrived,
              server, redirect_url);