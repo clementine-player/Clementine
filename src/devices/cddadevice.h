9d4aa94aa4881bbf5b8fef90a23cd993408f37de
kentsangkm <kentsangkm@pm.me>
2021-06-30 21:15:53 +0200

    clang-format

--- src/devices/cddadevice.h
+++ src/devices/cddadevice.h
@@ -45,7 +45,9 @@ class CddaDevice : public ConnectedDevice {
 
   bool Init() override;
   bool CopyToStorage(const MusicStorage::CopyJob&) override { return false; }
-  bool DeleteFromStorage(const MusicStorage::DeleteJob&) override { return false; }
+  bool DeleteFromStorage(const MusicStorage::DeleteJob&) override {
+    return false;
+  }
   CddaSongLoader* loader();
   // Access to the raw cdio device handle.
   CdIo_t* raw_cdio();  // TODO: not ideal, but Ripper needs this currently
